0dbde20f71b8a4750ff61a9dfcb164499d0d7d1f
tparadis <theophile.paradis@gmail.com>
2016-11-23 17:54:22 +0100

    Bipeur play

--- src/vue/BipeurImpl.java
+++ src/vue/BipeurImpl.java
@@ -1,17 +1,24 @@
 package vue;
 
+import java.applet.Applet;
+
 /**
  * Created by ce on 16/11/16.
  */
 public class BipeurImpl implements Bipeur {
 
+	java.net.URL temps = getClass().getResource("/view/Beep.wav");
+	java.applet.AudioClip sonTemps = Applet.newAudioClip(temps);
+	java.net.URL mesure = getClass().getResource("/view/Beep.wav");
+	java.applet.AudioClip sonMesure = Applet.newAudioClip(mesure);
+
 	@Override
 	public void marquerTemps() {
-
+		sonTemps.play();
 	}
 
 	@Override
 	public void marquerMesure() {
-
+		sonMesure.play();
 	}
 }
